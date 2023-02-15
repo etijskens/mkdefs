35917f2fb243cb653a935d2a0f2f031fc2bc8c32
Bert Tijskens <engelbert.tijskens@uantwerpen.be>
2023-02-15 23:15:48 +0100

    first working version

--- mkdefs/__init__.py
+++ mkdefs/__init__.py
@@ -4,19 +4,177 @@
 Package mkdefs
 =======================================
 
-Top-level package for mkdefs.
+find all occurences of bold text in all .md files listed in mkdocs.yml. and the heading in which they appear
+produce an alphabetic list of them and links to the heading
 """
 
-__version__ = "0.0.0"
+__version__ = "0.1.0"
 
+from typing import Union
+from pathlib import Path
+import yaml
+import re
 
-def hello(who='world'):
-    """'Hello world' method.
-
-    :param str who: whom to say hello to
-    :returns: a string
+def make_glossary( path_to_project: Path
+                 , ignore: list = []
+                 , out: str ='glossary.md'
+                 , verbose: bool = False
+                 ):
+    """Extract a glossary from docs/*.md. Terms are assumed to be marked as '**Bold face text**'.
+    For every term a link is created to the section in which the term is appearing. The links are
+    nicely formatted in a glossary.md file.
     """
-    result = "Hello " + who
-    return result
+    path_to_project = Path(path_to_project)
+    if not path_to_project.is_dir():
+        raise FileNotFoundError(f"'{path_to_project}' is not a directory.")
 
-# Your code here...
+    ignore.append(out)
+    md_files = list_docs(path_to_project / 'mkdocs.yml')
+    links = parse(md_files, ignore=ignore)
+    lines = format_links(links)
+    if verbose:
+        for line in lines:
+            print(line)
+
+    with open(path_to_project / 'docs' / out, mode='w') as f:
+        for line in lines:
+            print(line, file=f)
+
+def list_docs(path_to_mkdocs_yml: Union[str,Path]) -> list:
+    """Read a mkdocs.yml file and return all files appearing under the 'nav:' entry.
+    """
+    # retrieve the filenames
+    p = Path(path_to_mkdocs_yml)
+    with open(p, "r") as f:
+        yaml_data = yaml.safe_load(f)
+    md_files = yaml_data['nav'] # a list of filenames
+
+    # convert the filenames to filepaths
+    p = p.parent / 'docs'
+    if not p.is_dir():
+        raise FileNotFoundError()
+    for i, filename in enumerate(md_files):
+        md_files[i] = p / filename
+
+    return md_files
+
+
+def parse(filepaths, ignore=[]) -> list[str]:
+    """Parse all filepaths not in ignore. Return a list of links."""
+    links = []
+    for p in filepaths:
+        if not p.name in ignore:
+            links.extend(parse_md(p))
+    return links
+
+
+def parse_md(file_md: Union[str,Path]) -> list:
+    """Parse a .md file, looking for '**bold face text**', identifying a term,
+    and the title of the paragraph in which the bold face text is found. The bold
+    face text may span two lines.
+
+    Return a list of markdown links in autorefs format:
+
+        '[bold face text][title-of-the-section-in-which-the-bold-face-text-is-found]'
+    """
+    with open(file_md,'r') as f:
+        lines = f.readlines()
+    current_title =  ''
+    links = []
+    l = 0
+    line = lines[l]
+    while l < len(lines):
+        if line.startswith('#'):
+            current_title = process_title(line)
+            line_done = True
+        else:
+            # look for "**<whatever>**"
+            rex_bf  =   r'\*\*[^*]+\*\*'       #  '**blabla bla**'
+            rex_bfs = r'\*\*\*[^*]+\*\*\*'     # '***blabla bla***'
+            bf_terms  = re.findall(rex_bf , line)
+            bfs_terms = re.findall(rex_bfs, line)
+            for bf_term in bf_terms:
+                if not f"*{bf_term}*" in bfs_terms:
+                    links.append(f"[{bf_term[2:-2]}][{current_title}]")
+                    line = line.replace(bf_term, '')
+            # look for '**<wathever>$'
+            rex_bf = r'\*\*.+$'
+            bf_terms  = re.findall(rex_bf , line)   #  '**blabla'
+            bfs_terms = re.findall(rex_bfs, line)   # '***blabla'
+            line_done = True # most probably, will be unset if not
+            for bf_term in bf_terms:
+                if not f"*{bf_term}" in bfs_terms:
+                    term0 = bf_term.strip()[2:]
+                    # find the second part on the next line, otherwise ignore
+                    l += 1
+                    line = lines[l]
+                    rex = r'^[^*]+\*\*'
+                    bf_terms = re.findall(rex, line)
+                    for bf_term in bf_terms:
+                        term1 = bf_term.strip()[:-2]
+                        links.append(f"[{term0} {term1}][{current_title}]")
+                        line = line[len(bf_term):]
+                        line_done = False
+        if line_done:
+            l += 1
+            try:
+                line = lines[l]
+            except IndexError:
+                pass
+    return links
+
+
+def process_title(line: str) -> str:
+    """Take a text line with a title or subtitle, remove the leading '#'-characters, strip(), remove
+    punctation and replace spaces with hyphens, convert to lowercase and return the resulting string.
+    """
+    while line[0] == '#':
+        line = line[1:]
+    line = line.strip()
+    # remove punctuation:
+    t = line
+    for c in ".,?!'":
+        t = t.replace(c, '')
+    # replace spaces with hyphens
+    t = t.replace(' ', '-')
+    # convert to lowercase:
+    t = t.lower()
+
+    return t
+
+
+def format_links(links: list[str]) -> list[str]:
+    """Take a list of links, put them in alphabetical order, add a title, and subtitles A, B, C, ...
+    Return a list of lines.
+    """
+    links = sorted(links, key=str.casefold)
+
+    prev = ''
+    lines = ["# Glossary"
+            ,""
+            ,"Here is an alphabetical list of terms with links to where they are explained in the text."
+            ]
+
+
+    for link in links:
+        c = link[1].upper()
+        if c != prev:
+            prev = c
+            lines.extend([ "", f"## {c}", ""])
+        lines.append(f"- {link}")
+
+    return lines
+
+
+if __name__ == "__main__":
+    make_glossary( '../../wetppr'
+                 , ignore=['index.md'
+                          ,'over-de-auteur.md'
+                          ,'overview.md'
+                          ,'evaluation.md'
+                          ,'assignment.md'
+                          ,"guide-lines.md"
+                          ]
+                 , verbose=True)
+
+    print("-*# finished #*-")
